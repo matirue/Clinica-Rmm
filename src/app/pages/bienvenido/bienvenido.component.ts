222401b5a5821eaa448b04413e42bf0e327b4600
matirue <matiasrue@gmail.com>
2024-07-04 00:32:24 -0300

    Subo primeros arreglos del spr2

--- src/app/pages/bienvenido/bienvenido.component.ts
+++ src/app/pages/bienvenido/bienvenido.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core'; 
+import { User } from 'src/app/clases/user';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-bienvenido',
@@ -6,6 +8,33 @@ import { Component, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./bienvenido.component.css']
 })
 export class BienvenidoComponent implements OnInit { 
+  
+  usuarioLogueado: User = new User();
+  ocultarBotonesLogueo: boolean = false;
+  usuario : User = new User();
 
-  ngOnInit(): void { }
+  constructor(
+    private authSvc: AuthService
+  ) { }
+
+  ngOnInit(): void {
+    this.authSvc.afAuth.authState.subscribe(res=>{
+      this.usuarioLogueado = JSON.parse(localStorage.getItem('usuarioLogueado'));
+      if(this.usuarioLogueado != null){
+
+        if(this.authSvc.isLogged == null){
+          this.ocultarBotonesLogueo = false;
+        }else{
+          this.ocultarBotonesLogueo = true;
+        }
+        if(res && res.uid){
+          this.ocultarBotonesLogueo = true;
+          this.usuario = this.authSvc.obtenerUsuario(res.email);
+          // console.log(this.usuario); 
+          
+        } 
+      } 
+      
+    });
+  }
 }
